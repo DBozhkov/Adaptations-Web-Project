3e15e64b5043696fff237c7678801db86d111b35
Dimitar Bozhkov <dimitarbzhkov@gmail.com>
2023-09-27 21:30:10 +0300

    Add Tests

--- Adaptations.Services.Data.Tests/MovieServiceTests.cs
+++ Adaptations.Services.Data.Tests/MovieServiceTests.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using System.Text;
@@ -47,6 +46,37 @@
             var expectedMovieName = this.DbContext.Movies.Select(x => x.MovieName).FirstOrDefault();
 
             Assert.Equal("Test Movie", expectedMovieName);
+            Assert.Equal(1, this.DbContext.Movies.Count());
+        }
+
+        [Fact]
+        public async Task CreateMovieShouldThrowExceptionIfNullValue()
+        {
+            var movieList = this.AddMultipleMovies();
+            foreach (var movie in movieList)
+            {
+                var counter = 1;
+                var userId = $"testUserId{counter}";
+                var imagePath = $"testImagePath{counter}";
+
+                await this.MoviesServiceMock.CreateAsync(movie, userId, imagePath);
+                counter++;
+            }
+
+            Assert.Equal(10, this.DbContext.Movies.Count());
+        }
+
+        [Fact]
+        public async Task AddingMultipleMoviesShouldWorkIfInputValuesAreCorrect()
+        {
+            var userId = "testUserId";
+            var imagePath = "testImagePath";
+            var testMovie = this.AddSingleMovie();
+
+            testMovie.MovieName = null;
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+             await this.MoviesServiceMock.CreateAsync(testMovie, userId, imagePath));
         }
 
         private CreateMovieInputModel AddSingleMovie()
@@ -90,6 +120,55 @@
             };
 
             return movie;
+        }
+
+        private List<CreateMovieInputModel> AddMultipleMovies()
+        {
+            var list = new List<CreateMovieInputModel>();
+            for (int i = 1; i <= 10; i++)
+            {
+                var image1 = new FormFile(Stream.Null, 0, 0, $"imageFile{i}", $"image{i}.jpg")
+                {
+                    Headers = new HeaderDictionary(),
+                    ContentType = "image/jpeg",
+                };
+                var image2 = new FormFile(Stream.Null, 0, 0, $"secondImageFile{i}", $"secondImage{i}.jpg")
+                {
+                    Headers = new HeaderDictionary(),
+                    ContentType = "image/jpeg",
+                };
+                var rand = new Random();
+
+                var movie = new CreateMovieInputModel
+                {
+                    MovieName = $"Test Movie{i}",
+                    DirectorName = $"Test Director Name{i}",
+                    ReleaseYear = 2000 + i,
+                    MoviePlot = $"Test Plot{i}",
+                    Genre = MovieGenre.Comedy,
+                    Rating = rand.Next(1, 10),
+                    RunTime = 111 + i,
+                    Actors = new List<ActorInputModel>
+                {
+                    new ActorInputModel
+                    {
+                    Name = $"Test Actor Name{i}",
+                    Biography = $"Test Actor Bio{i}",
+                    },
+                    new ActorInputModel
+                    {
+                        Name = $"Second Test Name{i}",
+                        Biography = $"Second Test Bio{i}",
+                    },
+                },
+                    BookTitle = $"Test BookTitle{i}",
+                    Images = new List<IFormFile> { image1, image2 },
+                };
+
+                list.Add(movie);
+            }
+
+            return list;
         }
     }
 }
